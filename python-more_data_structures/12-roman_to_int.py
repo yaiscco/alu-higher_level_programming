6d9c6ba3733eb4da302fb7ef42a151f7b1006098
yaiscco <yaimajakacol@gmail.com>
2025-10-30 17:00:03 -0700

    WE'RE DONEE!

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    # Mapping of Roman numerals to their integer values
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        current_value = roman_values.get(char, 0)
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            total += current_value  # Add otherwise
+        prev_value = current_value
+
+    return total
